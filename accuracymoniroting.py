f6b5c428f27684cfb8e8cc5e3a83119311bc8fa2
sairamyalamarthi3-gif <sairamyalamarthi3@gmail.com>
2025-12-23 07:09:55 +0000

    Update accuracymoniroting.py

---
+++ accuracymoniroting.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import sklearn
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+st.title("Model Accuracy Monitoring")
+##base line accuracy
+baseline_accuracy = 0.90
+##Simulated production data
+y_true_prod = [1,0,1,1,0,1,0,1]
+y_pred_prod = [1,0,0,1,0,1,0,0]
+## Calculate production accuracy
+prod_accuracy= accuracy_score(y_true_prod,y_pred_prod)
+
+st.metric("Baseline Accuracy",baseline_accuracy)
+st.metric("Production Accuracy",prod_accuracy)
+
+threshold = 0.05
+
+if prod_accuracy < baseline_accuracy - threshold:
+    st.error("Model accuracy dropped in production")
+else:
+    st.success("Model accuracy is stable")
