6a7fafc0b3720a92f16c2a0235abba029f00e3ac
sairamyalamarthi3-gif <sairamyalamarthi3@gmail.com>
2025-12-25 21:30:23 +0000

    Create pagevisit.py

---
+++ pagevisit.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+st.title("Real Time Page Monitoring System")
+
+## Initialize Session State
+if "page_visits" not in st.session_state:
+  st.session_state.page_visits = 0
+
+##Simulate a page visit
+if st.button("Simulate Page Visit"):
+  st.session_state.page_visits += 1 
+
+## Display Real time metric
+st.metric(
+  label = "Total Page Visits",
+  value = st.session_state.page_visits
+)
+
+st.write("Current Visit Count": st.session_state.page_visits)
