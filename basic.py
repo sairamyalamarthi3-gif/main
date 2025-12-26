e25a7a8d96216113c5395efb576efb658d925d89
Sairam Yalamarthi <sairamyalamarthi3@gmail.com>
2025-12-26 13:47:23 +0000

    Update basic.py

--- basic.py
+++ basic.py
@@ -9,3 +9,21 @@ if primary_button:
    st.write("Hello from primary")
 if secondary_button:
    st.write("Hello from Secondary")
+st.divider()
+## checkbox
+checkbox = st.checkbox("Remember me")
+if checkbox:
+   st.write("I will remember you")
+else:
+   st.write("I will not remember you")
+
+st.divider()
+##Radio Buttons
+df = pd.read_csv("data.csv")
+radio = st.radio("Choose a column",options = df.columns[1:], index = 1, horizontal = False)
+st.write(radio)
+
+st.divider()
+##Select box
+select= st.selectbox("Choose a column", options = df.columns[1:].index = 1, vertical = True)
+st.write(select)
