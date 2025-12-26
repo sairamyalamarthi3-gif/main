6be9b9e42111e62c6f36ea4e69b7de970b6b5897
Sairam Yalamarthi <sairamyalamarthi3@gmail.com>
2025-12-26 13:41:16 +0000

    Update basic.py

--- basic.py
+++ basic.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 
 ##Buttons
-primary button = st.button("Primary", type = "primary")
-secondary button = st.button("Secondary", type = "secondary")
+primary_button = st.button("Primary", type = "primary")
+secondary_button = st.button("Secondary", type = "secondary")
 
-if primary button:
+if primary_button:
    st.write("Hello from primary")
-if secondary button:
+if secondary_button:
    st.write("Hello from Secondary")
