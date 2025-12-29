17905542506f3b553166f2ab6c89e73762f9254f
Sairam Yalamarthi <sairamyalamarthi3@gmail.com>
2025-12-29 19:51:06 +0000

    Update websitemonitor.py

--- websitemonitor.py
+++ websitemonitor.py
@@ -9,7 +9,7 @@ st.title("🌐 Real-Time Website Traffic Monitoring")
 # Simulate visitor timestamps
 np.random.seed(42)
 timestamps = [datetime.now() - timedelta(minutes=i) for i in range(30)]
-counts = np.random.randint(5, 20, size=10)
+counts = np.random.randint(5, 20, size=30)
 
 df = pd.DataFrame({"timestamp": timestamps[::-1], "visits": counts})
 
